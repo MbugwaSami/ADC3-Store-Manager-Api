66fadf14831f7e53ada21eef7bb339d38831bfb6
mbugwasami <samimbugwa@gmail.com>
2018-10-25 15:56:49 +0300

    [Chore #161469685] Create Connection

--- instance/config.py
+++ instance/config.py
@@ -1,27 +1,35 @@
+import os
+
 class Config(object):
     "parent configuration class"
     DEBUG= False
 
 
+
 class DevelopmentConfig(Config):
     "Configurations for Development"
     DEBUG = True
-
+    connectionVariables="dbname='store-manager' user='postgres' password='mwoboko10@' host='localhost' port='5432'"
+    os.environ['ENV']='testing'
 
 class TestingConfig(Config):
     """Configurations for Testing,"""
     TESTING = True
     DEBUG = True
+    connectionVariables="dbname='store-manager-test' user='postgres' host='localhost' port='5432'"
+    os.environ['ENV']='development'
 
 class StagingConfig(Config):
     """Configurations for Staging."""
     DEBUG = True
+    os.environ['ENV']='staging'
 
 
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+    os.environ['ENV']='production'
 
 
 app_config = {
