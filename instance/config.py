d59fef41e5d41fb7646dd168116e537a1713288e
mbugwasami <samimbugwa@gmail.com>
2018-11-15 08:27:20 +0300

    [Bug #161874349] Change test db

--- instance/config.py
+++ instance/config.py
@@ -11,14 +11,14 @@ class DevelopmentConfig(Config):
     "Configurations for Development"
     DEBUG = True
     connectionVariables="dbname='Database_store' user='postgres' password='Mwoboko10@' host='localhost' port='5432'"
-    # os.environ['ENVIRONMENT']='development'
+    os.environ['ENVIRONMENT']='development'
 
 class TestingConfig(Config):
     """Configurations for Testing,"""
     TESTING = True
     DEBUG = True
     connectionVariables="dbname='store_manager_test' password='Mwoboko10@' user='postgres' host='localhost' port='5432'"
-    # os.environ['ENVIRONMENT']='testing'
+    os.environ['ENVIRONMENT']='testing'
 
 
 app_config = {
